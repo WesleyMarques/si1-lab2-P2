b059fead1f38f8c70e753ab52d6faa4384573938
WesleyMarques <wesley.torres@ccc.ufcg.edu.br>
2014-06-15 12:27:09 -0300

    inclusão da tabela em HTML das metas semanais

--- app/models/Metas.java
+++ app/models/Metas.java
@@ -9,9 +9,22 @@ import play.db.ebean.Model;
 @Entity
 public class Metas extends Model {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	private long id;
 
 	@Required
 	private int priority;// vai de 0 - 5
+	
+	@Required
+	private String nameMeta;
+	
+	@Required
+	private int week; //0-6
+	
+	private boolean isDone;
 }
